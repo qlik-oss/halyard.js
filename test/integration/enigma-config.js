0ea61f15435e6099bf0049ad5dbca2cd0fe06b95
Stefan Enberg <Stefan.Enberg@qlik.com>
2017-10-31 16:04:19 +0100

    removed session from enigma config

--- test/integration/enigma-config.js
+++ test/integration/enigma-config.js
@@ -5,11 +5,6 @@ import qixSchema from 'enigma.js/schemas/3.2.json';
 const config = {
   Promise: bluebird,
   schema: qixSchema,
-  session: {
-    port: '4848',
-    secure: false,
-    disableCache: true,
-  },
   createSocket(url) {
     return new WebSocket(url);
   },
